605b29a17b35467fd1c3cc8589b151c15ce24d66
ADIL <116960478+adil3319@users.noreply.github.com>
2025-11-08 19:49:18 -0300

    Update likelihood.py

--- src/jimgw/single_event/likelihood.py
+++ src/jimgw/single_event/likelihood.py
@@ -314,8 +314,8 @@ class HeterodynedTransientLikelihoodFD(TransientLikelihoodFD):
         ####### lines modified to make the waveform zero above maximum amplitude##
         cf =1.4765e3
         c1=2.998e8
-       # f_maximum = 0.018/((self.ref_params["M_c"]/self.ref_params["eta"]**0.6)*(cf/c1))
-        f_maximum = 2000
+        f_maximum = 0.018/((self.ref_params["M_c"]/self.ref_params["eta"]**0.6)*(cf/c1))
+       # f_maximum = 2000
         # Compute index where frequency exceeds f_maximum
         cutoff_index = jnp.argmax(frequency_original > f_maximum)
         # If f_maximum is beyond the frequency range, don't apply zeroing
