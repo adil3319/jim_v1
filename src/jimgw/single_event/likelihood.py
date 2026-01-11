39c89ca18cf2cfacf4594bafb909d7bb0f3e9b79
ADIL <116960478+adil3319@users.noreply.github.com>
2026-01-11 17:11:46 +0530

    Update likelihood.py

--- src/jimgw/single_event/likelihood.py
+++ src/jimgw/single_event/likelihood.py
@@ -322,7 +322,7 @@ class HeterodynedTransientLikelihoodFD(TransientLikelihoodFD):
         # Handle case where cutoff is beyond frequency range
         if frequency_original[-1] <= f_maximum:
                    cutoff_index_general = None
-        if frequency_original[-1] <= (f_maximum / 2):
+        if frequency_original[-1] <= (f_maximum): ## for l = m = 2
                    cutoff_index_breathing = None
         # Zero-out modes above their respective cutoff frequencies
         for key in h_sky.keys():
